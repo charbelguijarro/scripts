945a0d1eb35e8b6ad6c8956164e61251adeb8760
Florian Coste <fcoste21@gmail.com>
2016-06-13 23:00:27 +0200

    Standardize case sensitivity

--- backup-dd.sh
+++ backup-dd.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-BACKUP_DATE=$(date +%Y%m%d)
-SRC=("/data" "/home/nairwolf")
-DEST=/media/nairwolf/b770cf56-f1e0-45ce-a608-5f1a0a56cfeb/backup/
+backup_date=$(date +%Y%m%d)
+src_dirs=("/data" "/home/nairwolf")
+dest=/media/nairwolf/b770cf56-f1e0-45ce-a608-5f1a0a56cfeb/backup/
 
-if [ ! -d "$DEST" ]
+if [ ! -d "$dest" ]
 then
 	echo "The back up hard drive is not present"
 	exit 1
 fi
 
-for src in "${SRC[@]}"
+for src in "${src_dirs[@]}"
 do
 	echo "**************"
 	echo "Back up the repository : '$src'"
-	NAME_SRC=$(basename $src)
-	BACKUP_REPORT="$DEST"backup-"$NAME_SRC"-"$BACKUP_DATE"
-	touch "$BACKUP_REPORT" 
+	name_src=$(basename $src)
+	backup_report="$dest"backup-"$name_src"-"$backup_date"
+	touch "$backup_report" 
 
-	if [ ! -f "$BACKUP_REPORT" ]
+	if [ ! -f "$backup_report" ]
 	then
-		echo "$BACKUP_REPORT can't be created"
+		echo "$backup_report can't be created"
 		exit 1
 	fi
 
-	rsync -a -v --delete --stats "$src" "$DEST" > "$BACKUP_REPORT"
+	rsync -a -v --delete --stats "$src" "$dest" > "$backup_report"
 
 	if [ "$?" -eq 0 ]
 	then
