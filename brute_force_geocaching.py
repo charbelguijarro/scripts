627505dafadd6378f960b36d4241fa163f9453ac
Florian Coste <fcoste21@gmail.com>
2016-07-14 20:18:19 +0200

    Fix brute force geocaching riddle

--- brute_force_geocaching.py
+++ brute_force_geocaching.py
@@ -11,7 +11,7 @@ def coord_to_hash(coord):
 
     return m.hexdigest()
 
-def createCoord(NCoord, ECoord, NBase='N 43 4', EBase='E 007 0'):
+def createCoord(NCoord, ECoord, NBase='N 43 3', EBase='E 007 0'):
     NStr = str(NCoord)
     EStr = str(ECoord)
     
@@ -20,41 +20,41 @@ def createCoord(NCoord, ECoord, NBase='N 43 4', EBase='E 007 0'):
     return Coord
 
 if __name__ == "__main__":
-    # top_right = "N 43 39.000 E 007 07.000"
-    # top_left = "N 43 39.000 E 007 01.000"
-    # bottom_left = "N 43 34.000 E 007 01.000"
-    # bottom_right = "N 43 34.000 E 007 07.000"
+    # top_right = "N 43 39.999 E 007 09.999"
+    # top_left = "N 43 39.999 E 007 00.000"
+    # bottom_left = "N 43 30.000 E 007 00.000"
+    # bottom_right = "N 43 30.000 E 007 09.999"
 
     start = time.time()
     
-    N_base = "N 43 3"
-    E_base = "E 007 0"
-
     sha = 'a90c2ef06f807056ca7566ce35e1cc247a16691c926403411fbff7d73d656b60a95dd5b3d0cfeae8ef9339f2cd64bb5e6770f70701d79e5e27655263cb556c0b'
     finalCoord = ''
 
     decimal.getcontext().prec = 4
-    NCoord = decimal.Decimal('4.000')
-    ECoord = decimal.Decimal('1.000')
+    NCoord = decimal.Decimal('0.000')
+    ECoord = decimal.Decimal('0.000')
     inc = decimal.Decimal('0.001')
+    isFound = False
 
-    for N in range(5000):
-        for E in range(6000):
-            ECoord += inc
+    for N in range(9999):
+        ECoord = decimal.Decimal('1.000')
+
+        for E in range(9999):
             Coord = createCoord(NCoord, ECoord)
             print(Coord)
-
             hashCoord = coord_to_hash(Coord)
 
             if hashCoord == sha:
+                isFound = True
                 finalCoord = Coord
                 print("************************")
                 break
 
+            ECoord += inc
+            
         if finalCoord != '':
             break
-        else:
-            NCoord += inc
+        NCoord += inc
 
     print("les coordonnées de la cache sont :")
     print(finalCoord)
@@ -62,4 +62,3 @@ if __name__ == "__main__":
     end = time.time()
     diff = int(end - start)
     print("le décodage a pris "+str(diff)+" secondes.")
-
