659a550e5f2b916835a3a32da6077689cd90eb57
Florian Coste <fcoste21@gmail.com>
2016-08-05 03:30:17 +0200

    First version of update-todolist script

---
+++ update-todolist.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import argparse
+from datetime import date, timedelta
+
+"""
+    todo-update
+    Python script to automatize my personal todo list
+"""
+
+def get_week_line():
+    today = date.today()
+    week = today.isocalendar()[1]
+    start_week = today + timedelta(days=today.day - 1)
+    end_week = start_week + timedelta(days=6)
+
+    line_to_print = "## Week {} - {}/{} -> {}/{}\n\n".format(week, start_week.day, 
+                                                            start_week.month,
+                                                            end_week.day,
+                                                            end_week.month)
+    return line_to_print
+
+def parse_args():
+    """
+        Parse args with argparse
+    """
+    parser = argparse.ArgumentParser(description='Create new paragraph for your own todolist')
+    parser.add_argument('todolist')
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    with open(args.todolist, 'r') as f:
+        lines = f.readlines()
+
+    lines[1] = get_week_line() + lines[1]
+
+    with open(args.todolist, 'w') as f:
+        f.write(''.join(lines))
