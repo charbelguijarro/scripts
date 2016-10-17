e67119fdbdd78dc273397877736b5f50f3b46272
Florian Coste <fcoste21@gmail.com>
2016-10-17 23:42:50 +0200

    Create new script to add current day of personal diary

---
+++ update_journal.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from datetime import date
+
+JOURNAL = '/home/nairwolf/Documents/misc/diary/journal.md'
+DAYS = ('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche')
+MONTHS = ('Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', \
+        'Septembre', 'Octobre', 'Novembre', 'Décembre')
+
+def main():
+    content = get_content(JOURNAL)
+    content = add_current_day(content)
+    write_content(JOURNAL, content)
+
+def get_content(filepath):
+    with open(filepath, 'r') as f:
+        content = f.read()
+    return content
+
+def add_current_day(content):
+    today = date.today()
+    weekday = DAYS[today.isoweekday()]
+    daynumber = today.day
+    month = MONTHS[today.month]
+
+    datestring = '# {} {} {}\n\n'.format(weekday, daynumber, month)
+
+    content = datestring + content
+    return content
+
+def write_content(filepath, content):
+    with open(filepath, 'w') as f:
+        f.write(content)
+
+if __name__ == '__main__':
+    main()
