635068fce8cc83b9a64913dc1ece12315eb1db16
Florian Coste <fcoste21@gmail.com>
2016-10-22 11:51:37 +0200

    Add new script to install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ sudo cp practice_chinese.py ${target}practice_chinese
 sudo cp share_expenses.py ${target}share_expenses
 sudo cp update-todolist.py ${target}update-todolist
 sudo cp update_journal.py ${target}update_journal
+sudo cp get_upstream_update.py ${target}get_upstream_update
 
 sudo chmod 755 ${target}*
 
