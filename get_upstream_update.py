b3a7e9571298a4fec806df0de33047d97c24dda6
Florian Coste <fcoste21@gmail.com>
2016-11-09 16:11:46 +0100

    Upstream-update: Create _cmp function to use TDD

--- get_upstream_update.py
+++ get_upstream_update.py
@@ -3,10 +3,29 @@
 import re
 import sys
 
+def _cmp(expected, result):
+    """ 
+        Returns True if expected == result
+        else returns False
+    """
+    if expected == result:
+        print("OK")
+    else:
+        print("FAIL")
+        minlen = min(len(result), len(expected))
+        for i in range(minlen):
+            if result[i] != expected[i]:
+                before = max(0, i-10)
+                after = min(minlen, i+50)
+                print("**********")
+                print("Should be: {}\nIs: {}".format(expected[before:], result[before:]))
+                break
+
+
 def get_upstream_update(text):
     """
     Take the output of the command `apt list --upgradable and returns
-    only upstream_updates
+    a list of upstream update
     text: 
         vim/zesty 2:7.5.1829-1ubuntu3 amd64 [upgradable from: 2:7.4.1829-1ubuntu2]
         vim-common/zesty 2:7.4.1829-1ubuntu3 amd64 [upgradable from: 2:7.4.1829-1ubuntu2]
@@ -15,7 +34,7 @@ def get_upstream_update(text):
     update:
         'xfce4-terminal' : 0.6.3 -> 0.8.1
     """    
-
+    upstream = []
     text = text.strip()
     pattern = (r'(.+)/\w+ (\d+\.\d+(?:\.\d+)?).* (?:amd64|i386) \[upgradable from: '
                r'(\d+\.\d+(?:\.\d+)?).*')
@@ -31,19 +50,22 @@ def get_upstream_update(text):
             new = rst[1]
             old = rst[2]
             if new != old:
-                print("'{}' : {} -> {}".format(package_name, 
-                                                                      old, new))
+                upstream.append("'{}' : {} -> {}".format(package_name, old, new))
+    return upstream
+
 if __name__ == '__main__':
     try:
         sys.argv[1] == 'DEBUG'
         text = """
-            vim/zesty 2:7.5.1829-1ubuntu3 amd64 [upgradable from: 2:7.4.1829-1ubuntu2]
             mysql-common/zesty 5.8+1.0.0ubuntu1 all [upgradable from: 5.7.15-0ubuntu2]
-            python3.5/zesty 3.5.2-7 amd64 [upgradable from: 3.5.2-6]
-            python3.5-dev/zesty 3.5.2-7 amd64 [upgradable from: 3.5.2-6]
+            xfce4-terminal/zesty 0.8.1-1 amd64 [upgradable from: 0.6.3-2ubuntu1]
             """
+        rst = "\n".join(get_upstream_update(text))
+        expected = """
+'mysql-common' : 5.7.15 -> 5.8
+'xfce4-terminal' : 0.6.3 -> 0.8.1"""
+        _cmp(expected, rst)
     except:
         text = sys.stdin.read()
-    finally:
-        get_upstream_update(text)
+        print("\n".join(get_upstream_update(text)))
 
