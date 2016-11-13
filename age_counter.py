a94b4012adee5f9feac999e6a467fe67105473b2
Florian Coste <fcoste21@gmail.com>
2016-11-13 23:45:56 +0100

    Age_counter: Use argparse module to create CLI command

--- age_counter.py
+++ age_counter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime as dt
+from argparse import ArgumentParser
 
 # BIRTHDAY = 'dd/mm/yyyy - Hhm'
 BIRTHDAY = '21/08/1991 - 19h30'
@@ -13,6 +14,11 @@ def parse_birthday():
     return birthday
 
 def get_diff(born):
+    """
+        compute age and time_diff between two dates. 
+        age <int> : difference in year between the two dates.
+        time_diff <datetime.timedelta> : difference the last birthday and now.
+    """
     now = dt.datetime.now()
     birthday = dt.datetime(now.year, born.month, born.day, born.hour, born.minute)
     last_birthday = birthday
@@ -45,7 +51,27 @@ def counter():
     precise_age = compute_age(age, time_diff)
     return precise_age
 
+def _parse_args():
+    parser = ArgumentParser(description='Compute precisely time between \
+    two dates, displayed in years.')
+
+    parser.add_argument('-f', 
+            dest='from_date',
+            default='1/1/1970',
+            help="The date 'from' with this syntax : '1/12/2000'",
+            metavar='date'
+            )
+
+    parser.add_argument('-t', 
+            dest='to_date',
+            help="The date 'to' with this syntax : '1/12/2000'",
+            metavar='date',
+            )
+
+    return parser.parse_args()
+
 if __name__ == '__main__':
+    args = _parse_args()
     age = counter()
     print("You have {} years old!".format(age))
 
