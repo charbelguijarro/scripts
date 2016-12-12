68aa607540ca7eca040b26460b9a12181d183be7
Florian Coste <fcoste21@gmail.com>
2016-12-12 17:50:20 +0100

    Vim_format: Manages line breaks correctly
    In the diary project, lines beginning with a hash should
    not be set to the same level as the rest of the lines.

--- vim_format.py
+++ vim_format.py
@@ -3,18 +3,33 @@
 import sys
 import textwrap
 
-WIDTH_MAX = 90
+WIDTH_MAX = 85
 
-def check_line(line):
-    rst = []
-    if len(line) <= WIDTH_MAX:
-        return [line]
-    else:
-        i = line.rfind(' ', 0, WIDTH_MAX)
-        l1 = line[:i]
-        rest_line = line[i+1:]
-        return [l1] + check_line(rest_line)
+def add_carriage_return(content):
+    lines = []
+    for line in content.splitlines():
+        newline = line
+        if line.startswith('#'):
+            newline = line + '\n' 
+        lines.append(newline)
 
+    return '\n'.join(lines)
+
+def remove_carriage_return(content):
+    lines = content.splitlines()
+    new_lines = [v for i, v in enumerate(lines) 
+                    if i == 0 or not lines[i-1].startswith('#')]
+    
+    """
+    lines = iter(content.splitlines())
+    new_lines = []
+    for line in lines:
+        new_lines.append(line)
+        if line.startswith('#'):
+            next(lines, False)
+
+    """
+    return '\n'.join(new_lines)
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
@@ -24,14 +39,18 @@ if __name__ == '__main__':
         exit()
 
     with open(filename, 'r') as fi:
-        contents = fi.read()
+        content = fi.read()
 
-    paragraphs = contents.split('\n\n')
+    content = add_carriage_return(content)
 
-    new_content = ''
-    for pr in paragraphs:
-        lines = textwrap.fill(pr, width=WIDTH_MAX) + '\n\n'
-        new_content += lines
+    paragraphs = content.split('\n\n')
+
+    wrapped_content = ''
+    for paragraph in paragraphs:
+        new_pr = textwrap.fill(paragraph, width=WIDTH_MAX) + '\n\n'
+        wrapped_content += new_pr
+
+    new_content = remove_carriage_return(wrapped_content)
 
     with open(filename, 'w') as fo:
         fo.write(new_content.strip())
